79b59e32de1721ab3fce9ca7d882aec207c1120a
sergueyarellano <sarellano85@gmail.com>
2022-04-04 17:47:16 -0400

    feat: add getVersion and cache features

---
+++ README.md
@@ -0,0 +1,22 @@
+aws-cache-schema
+===
+
+aws-cache-schema will check if the definition of the schema is cached and will get it and cache it for you if not present
+
+```js
+import cacheSchema from '@sergueyarellano/aws-cache-schema'
+
+// initialize the cache
+const cache = cacheSchema('default-registry', 'us-west-2')
+
+// lambda function
+export const handler = async event => { 
+  const SCHEMA_NAME = 'userSchema'
+  const VERSION = 1
+
+  const schemaDefinition = await cache.getSchema(SCHEMA_NAME, version)
+}
+```
+
+
+you need `aws-sdk` as a dependency for this package to work
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import * as AWS from 'aws-sdk'
+
+interface CacheSchema {
+  schemas: CachedSchemas
+  getVersion: (version: number, schemaName: string) => Promise<string>
+}
+
+interface CachedSchemas {
+  [key: string]: CachedVersions
+}
+
+interface CachedVersions {
+  [key: number]: string
+}
+
+function cacheSchema (RegistryName: string, region: string): CacheSchema {
+  const glue = new AWS.Glue({ region })
+  return {
+    schemas: {},
+    getVersion: async function getVersion (VersionNumber, SchemaName) {
+      const schema = this.schemas
+      let definition = ''
+      if (schema[SchemaName] == null) {
+        this.schemas[SchemaName] = {}
+      } else if (schema[SchemaName][VersionNumber] != null) {
+        definition = schema[SchemaName][VersionNumber]
+      } else {
+        // get Schema from glue
+        const params = {
+          SchemaId: {
+            RegistryName,
+            SchemaName
+          },
+          SchemaVersionNumber: {
+            VersionNumber
+          }
+        }
+        const data = await glue.getSchemaVersion(params).promise().catch((e: Error) => { console.log(e); return null })
+        if (data == null || data.SchemaDefinition == null) {
+          throw new Error(`Could not find the Schema ${SchemaName}, with version ${VersionNumber.toString()}`)
+        }
+        definition = data.SchemaDefinition
+        // cache the version
+        this.schemas[SchemaName][VersionNumber] = definition
+      }
+
+      return definition
+    }
+  }
+}
+
+export default cacheSchema
